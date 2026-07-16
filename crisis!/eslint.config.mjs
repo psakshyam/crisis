3731a4ed7542b09bdd3f36f9b19e1bb6e1667fc7
Sakshyam Pokharel <sp2297@princeton.edu>
2026-07-16 09:20:47 -0400

    fix

--- crisis!/eslint.config.mjs
+++ crisis!/eslint.config.mjs
@@ -1,15 +1,23 @@
 import js from "@eslint/js";
 import prettier from "eslint-plugin-prettier/recommended";
+import globals from "globals";
 
 export default [
   { ignores: ["dist"] },
+  js.configs.recommended,
+  prettier,
   {
-    extends: [js.configs.recommended, prettier],
-    files: ["**/*.{js,jsx}"],
+    files: ["**/*.{js,jsx,cjs,mjs}"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
-    rules: {},
+    rules: {
+      "prettier/prettier": "warn",
+    },
   },
 ];
